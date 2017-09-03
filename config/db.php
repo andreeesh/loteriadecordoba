4b1746105d69fa786a303c4fdabd69755d7849a8
Andres <andreeesh@gmail.com>
2017-09-03 14:10:04 -0300

    Instalación de driver Oracle y configuracion de conexión

--- config/db.php
+++ config/db.php
@@ -1,9 +1,8 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+	'class' => '\sfedosimov\oci8pdo\Oci8PDO_Connection',
+	'dsn'=>'oci:dbname=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=webdb)(PORT=1522))(CONNECT_DATA=(SID=WST)));charset=AL32UTF8;',
+	'username'=>'web01',
+	'password'=>'web02',
 ];
