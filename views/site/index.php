6807e6d0bdd0d68865f9f002fc6daea0b99b4aad
Andres <andreeesh@gmail.com>
2017-09-03 18:42:37 -0300

    Armo una lista para el menú de los juegos

--- views/site/index.php
+++ views/site/index.php
@@ -1,1 +1,11 @@
-<?php $this->title='Lotería de Córdoba' ?>
+<?php $this->title = 'Lotería de Córdoba' ?>
+
+<div class="site-index">
+<ul>
+<?php 
+foreach($juegos as $row):
+echo '<li><a href="index.php?r=site/juego&id='.$row["IDJUEGO"].'&sorteo=0">'.$row["NOMBRE"].'</a></li>';
+endforeach; 
+?>
+</ul>
+</div>
